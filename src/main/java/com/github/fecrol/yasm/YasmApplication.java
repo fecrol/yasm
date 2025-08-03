7f35e163797575de373c115b48a2af2e9571daa0
Maciej Fec <maciejfec1996@gmail.com>
2025-08-03 12:21:05 +0100

    feat: initial commit

---
+++ src/main/java/com/github/fecrol/yasm/YasmApplication.java
@@ -0,0 +1,13 @@
+package com.github.fecrol.yasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fecrol/yasm/YasmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fecrol.yasm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
