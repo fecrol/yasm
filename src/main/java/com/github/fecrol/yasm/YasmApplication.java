d816ca5a78d729390c373ec88068839cd87825e2
Maciej Fec <maciejfec1996@gmail.com>
2025-08-03 19:27:54 +0100

    feat YASM-4: enable jpa auditing to auto update updated_at column in db

--- src/main/java/com/github/fecrol/yasm/YasmApplication.java
+++ src/main/java/com/github/fecrol/yasm/YasmApplication.java
@@ -2,8 +2,10 @@ package com.github.fecrol.yasm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YasmApplication {
 
 	public static void main(String[] args) {
